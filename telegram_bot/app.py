8544853a2c2a9f0673e1f6510d4e509b1a55dc44
VladimirNesterenko1 <119951599+VladimirNesterenko1@users.noreply.github.com>
2023-01-29 18:55:03 +0300

    Add files via upload

---
+++ telegram_bot/app.py
@@ -0,0 +1,42 @@
+import telebot
+from config import keys, TOKEN
+from utils import ConvertionException, CryptoConverter
+
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands = ['start', 'help'])
+def help(message: telebot.types.Message):
+    text = "Чтобы начать работы, введите комманду в следующем формате: \n<название валюты (с большой буквы)>  \
+<в какую валюту перевести (с большой буквы)>  \
+<количесво переводимой валюты (число)>\nУвидеть список доступных валют: /values"
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands = ['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
+
+@bot.message_handler(content_types = ['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException("Слишокм много параметров")
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f"Ошибка пользователя.\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду\n{e}")
+    else:
+        text = f"Цена {amount} {quote} в {base} - {total_base} {keys[base]}"
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ telegram_bot/config.py
@@ -0,0 +1,9 @@
+TOKEN = '5942809111:AAEwhXv2C3MgXh1CQxDtEPylZDmXgLvvqrk'
+
+keys = {
+    'Биткоин': 'BTC',
+    'Эфириум': 'ETH',
+    'Доллар': 'USD',
+    'Евро': 'EUR',
+    'Рубль': 'RUB',
+}
